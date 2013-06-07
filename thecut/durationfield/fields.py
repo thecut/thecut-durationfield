b79472a52d684ff340478af8b6c4912c9ae4ac74
Elena Williams <elena.williams@thecut.net.au>
2013-06-07 16:45:33 +0800

    Add utils.isodate_to_reldelta to DurationField.to_python field. Fixes #1691. Refs #1689.

--- thecut/durationfield/fields.py
+++ thecut/durationfield/fields.py
@@ -2,7 +2,9 @@
 from __future__ import absolute_import, unicode_literals
 from django.db.models.fields import CharField
 from django.utils.translation import ugettext_lazy as _
-from django.utils.encoding import python_2_unicode_compatible, smart_text
+from django.utils.encoding import python_2_unicode_compatible
+
+from thecut.durationfield.utils import isodate_to_relativedelta
 
 try:
     from south.modelsinspector import add_introspection_rules
@@ -26,9 +28,16 @@ class DurationField(CharField):
         kwargs['max_length'] = 64
         super(DurationField, self).__init__(*args, **kwargs)
 
-
     def get_internal_type(self):
         return "CharField"
+
+    def to_python(self, value):
+        """
+        :py:class:`dateutil.relativedelta.relativedelta` is returned
+        from ISO8601 duration formatted string or
+        :py:class:`isodate.duration.Duration`.
+        """
+        return isodate_to_relativedelta(value)
 
 
 if add_introspection_rules:
