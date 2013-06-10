d33248f3b5ce863e94428b47288e7408b713f024
Josh Crompton <josh.crompton@thecut.net.au>
2013-06-10 16:34:26 +0800

    Add setup.py to make package installable via pip.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+from version import get_git_version
+
+setup(name='thecut-durationfield',
+    author='The Cut', author_email='development@thecut.net.au',
+    url='http://projects.thecut.net.au/projects/thecut-durationfield',
+    namespace_packages=['thecut'],
+    version=get_git_version(),
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['distribute'],
+)
