d5e1af1a20f104887e2243b4dd8ca0fa27f436cb
Josh Crompton <josh.crompton@thecut.net.au>
2013-06-14 11:12:54 +0800

    Set up tests.

---
+++ tests/tests.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from unittest import TestCase
+from thecut.durationfield.fields import ISO8601DurationField
+import isodate
+
+
+class TestISO8061DurationField(TestCase):
+
+    def test_can_run_a_test(self):
+        pass
