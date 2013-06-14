6b7cac2651fb3b4f2fbf9e7b8aabfb7af1663333
Josh Crompton <josh.crompton@thecut.net.au>
2013-06-14 11:30:41 +0800

    Ensure the duration is stored and retrieved as expected.

--- tests/tests.py
+++ tests/tests.py
@@ -1,11 +1,45 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from unittest import TestCase
-from thecut.durationfield.fields import ISO8601DurationField
+from django.core.exceptions import ValidationError
 import isodate
+from thecut.durationfield.fields import ISO8601DurationField
+from unittest import TestCase
 
 
 class TestISO8061DurationField(TestCase):
 
-    def test_can_run_a_test(self):
-        pass
+    def setUp(self):
+        self.field = ISO8601DurationField()
+
+    def test_to_python_returns_a_duration_when_given_a_duration(self):
+
+        duration = isodate.parse_duration('P1M')
+        self.assertEqual(type(duration), type(self.field.to_python(duration)))
+
+    def test_to_python_returns_a_duration_when_given_none(self):
+
+        self.assertEqual(isodate.duration.Duration,
+                         type(self.field.to_python(None)))
+
+    def test_to_python_returns_a_duration_when_given_an_empty_string(self):
+
+        self.assertEqual(isodate.duration.Duration,
+                         type(self.field.to_python('')))
+
+    def test_to_python_returns_a_duration_when_given_a_validly_formatted_string(self):
+
+        self.assertEqual(isodate.duration.Duration,
+                         type(self.field.to_python('P1M')))
+
+    def test_to_python_raises_an_error_when_given_an_invalid_string(self):
+
+        self.assertRaises(ValidationError, self.field.to_python, 'abc')
+
+    def test_get_prep_value_raises_an_error_when_called_with_an_invalid_object(self):
+
+        self.assertRaises(ValidationError, self.field.get_prep_value, 'abc')
+
+    def test_get_prep_value_is_isomorphic(self):
+
+        duration = isodate.parse_duration('P1M')
+        self.assertEqual('P1.0M', self.field.get_prep_value(duration))
--- thecut/durationfield/fields.py
+++ thecut/durationfield/fields.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from django.db.models.fields import CharField
+from django.db.models import Field, SubfieldBase
 from django.utils.translation import ugettext_lazy as _
 from django.utils.encoding import python_2_unicode_compatible
-from django.forms.util import ValidationError
+from django.core.exceptions import ValidationError
 from isodate.isoerror import ISO8601Error
+import isodate
 from isodate import duration_isoformat
-from thecut.durationfield.utils import isodate_to_relativedelta
 
 try:
     from south.modelsinspector import add_introspection_rules
@@ -15,11 +15,14 @@ except ImportError:
 
 
 @python_2_unicode_compatible
-class DurationField(CharField):
-    """Field (extended CharField) to accept ISO 8601 defined representation.
+class ISO8601DurationField(Field):
+    """Store and retrieve ISO 8601 formatted durations.
+
     """
 
-    description = _("Duration of time in ISO 8601 representation")
+    description = _("A duration of time (ISO 8601 format)")
+
+    __metaclass__ = SubfieldBase
 
     default_error_messages = {
         'invalid': _("This value must be in ISO 8601 Duration format."),
@@ -27,27 +30,41 @@ class DurationField(CharField):
     }
 
     def __init__(self, *args, **kwargs):
-        kwargs['max_length'] = 64
-        super(DurationField, self).__init__(*args, **kwargs)
+        self.max_length = kwargs['max_length'] = 64
+        super(ISO8601DurationField, self).__init__(*args, **kwargs)
+
+    def db_type(self, connection):
+        return 'char({0})'.format(self.max_length)
 
     def get_internal_type(self):
-        return "CharField"
+        return 'CharField'
 
     def to_python(self, value):
         """
-        :py:class:`dateutil.relativedelta.relativedelta` is returned
-        from ISO8601 duration formatted string or
-        :py:class:`isodate.duration.Duration`.
+        :returns: A duration object parsed from the given value.
+        :rtype: :py:class:`~isodate.duration.Duration`
+
         """
+        if isinstance(value, isodate.duration.Duration):
+            return value
+
+        if value is None or value == '':
+            return isodate.duration.Duration()
         try:
-            isodate = isodate_to_relativedelta(value)
+            duration = isodate.parse_duration(value)
         except ISO8601Error:
             raise ValidationError(self.default_error_messages['invalid'])
-        return isodate
+        return duration
 
     def get_prep_value(self, value):
-        return duration_isoformat(value)
+        if not isinstance(value, isodate.duration.Duration):
+            raise ValidationError('Cannot convert objects that are not Durations.')
+
+        return isodate.duration_isoformat(value)
+
+    def value_to_string(self, obj):
+        value = self._get_val_from_obj(obj)
+        return self.get_prep_value(value)
 
 if add_introspection_rules:
     add_introspection_rules([], ["^thecut\.durationfield\.fields"])
-
