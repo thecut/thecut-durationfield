04b4e8148fee0d6101854b5c5cee3a8ef421e694
Kye Russell <kye.russell@thecut.net.au>
2016-06-30 14:31:10 +0800

    Added package metadata.

--- thecut/exampleapp/__init__.py
+++ thecut/exampleapp/__init__.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
+__title__ = 'thecut-exampleapp'
+
+__version__ = '0.1'
+
+__url__ = 'https://github.com/thecut/thecut-exampleapp'
+
+__author__ = 'The Cut Creative <development@thecut.net.au>'
+
+__copyright__ = 'Copyright 2016 Busara Perth Pty Ltd'
+
+__license__ = 'Apache 2.0'
 
 default_app_config = 'thecut.exampleapp.apps.AppConfig'
