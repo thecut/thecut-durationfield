820f0a542f308b53d9cdc633695fb0f191ee67dd
Kye Russell <kye.russell@thecut.net.au>
2016-06-30 14:30:43 +0800

    Added Read The Docs sphinx theme to setup requirements.

--- setup.py
+++ setup.py
@@ -1,15 +1,63 @@
+from __future__ import print_function
+import codecs
+import io
+import os
+from thecut.exampleapp import __version__
 from setuptools import setup, find_packages
-from version import get_git_version
+import sys
 
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.rst', 'HISTORY.rst')
 
 setup(
+    # General information
     name='thecut-exampleapp',
-    author='The Cut',
-    author_email='development@thecut.net.au',
-    url='https://projects.thecut.net.au/projects/thecut-exampleapp',
-    namespace_packages=['thecut'],
-    version=get_git_version(),
+    version=__version__,
+
+    # Packaging
     packages=find_packages(),
+    namespace_packages=['thecut'],
     include_package_data=True,
-    install_requires=[''],
+
+    # Dependencies
+    install_requires=['sphinx_rtd_theme>=0.1.10a0'],
+
+    # Additional information
+    url='https://projects.thecut.net.au/projects/thecut-exampleapp',
+    license='Apache Software License 2.0',
+    author='The Cut Creative',
+    author_email='development@thecut.net.au',
+    description='A reusable application.',
+    long_description=long_description,
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Natural Language :: English',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Libraries :: Application Frameworks',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
 )
