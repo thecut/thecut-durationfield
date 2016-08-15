e1f42c7a1b466f91cc7814f6652ac2a1da74b846
Kye Russell <kye.russell@thecut.net.au>
2016-08-15 11:37:16 +1000

    Version bump

--- thecut/durationfield/__init__.py
+++ thecut/durationfield/__init__.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, unicode_literals
 
 __title__ = 'thecut-durationfield'
 
-__version__ = '2.0'
+__version__ = '2.0.1'
 
 __url__ = 'https://github.com/thecut/thecut-durationfield'
 
